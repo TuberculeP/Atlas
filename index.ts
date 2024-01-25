fec017817f5f2b92cb4ccaf8df7222c809381d7a
Félix Laviéville <lavieville.felix@gmail.com>
2024-01-25 12:31:21 +0100

    refacto

--- index.ts
+++
@@ -1,6 +0,0 @@
-/*
-Recette 1 : 1 Cuivre, 1 Ferrite pure -> 1 Metal chromatique
-Recette 2 : 1 Cuivre, 1 Metal chromatique -> 2 Cuivre
-Recette 3 : 2 Poussiere ferrite, 1 Pugneum -> 3 Ferrite pure
- */
-
